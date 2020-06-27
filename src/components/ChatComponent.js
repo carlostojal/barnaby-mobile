b6bd2ef1b655e63f046f22f195100abf3b1a6680
Carlos Tojal <carlos.tojal@hotmail.com>
2020-06-27 11:57:40 +0100

    show icon on weather requests

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { View, ScrollView, Text, StyleSheet } from 'react-native'
+import { View, ScrollView, Text, Image, StyleSheet } from 'react-native'
 import containers from '../style/containers.js'
+import text from '../style/text.js'
 
 class ChatComponent extends Component {
 
@@ -13,7 +14,12 @@ class ChatComponent extends Component {
 					{
 						messages.map((item, index) => (
 							<View key = {index} style = {item.sender == "bot" ? containers.botMessageContainer : containers.userMessageContainer}>
-								<Text style = {item.sender == "bot" ? containers.botMessage : containers.userMessage}> {item.content.response} </Text>
+								<View style = {item.sender == "bot" ? containers.botMessage : containers.userMessage}>
+									{item.content.functionality == "weather" &&
+										<Image source = {{uri: "https://openweathermap.org/img/wn/" + item.content.content.weather[0].icon + "@2x.png"}} style = {containers.weatherImage} />
+									}
+									<Text style = {item.sender == "bot" ? text.botMessage : text.userMessage}> {item.content.response} </Text>
+								</View>
 							</View>
 						))
 					}
